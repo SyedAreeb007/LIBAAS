d0e0e0f67c91c1fd7e16921d007f8a537f2d114d
SyedAreeb007 <144684190+SyedAreeb007@users.noreply.github.com>
2023-12-13 10:06:12 +0500

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      
+      <div className='hero'>
+            <div className="hero-left">
+                <h2>NEW ARRIVALS ONLY</h2>
+                <div>
+                    <div className='hero-hand-icon'>
+                        <p>new</p>
+                        <img src={hand_icon} />
+                    </div>
+                    <p>collections</p>
+                    <p>for everyone</p>
+                </div>
+                <div className="hero-latest-btn">
+                    <div>Latest Collection</div>
+                    <img src={arrow_icon} />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
