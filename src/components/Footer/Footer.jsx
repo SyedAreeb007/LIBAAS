d570b8a899cbcc8004fc375c72b2c796b6ab9982
Sadia Dayo <144684446+SadiaDayo@users.noreply.github.com>
2023-12-13 10:18:22 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      <div className='footer'>
+            <div className="footer-logo">
+                <img src={footer_logo} />
+                <p>SHOPPER</p>
+            </div>
+            <ul className="footer-links">
+                <Link style={{textDecoration: 'none'}} to='/about'><li>About</li></Link> 
+                <Link style={{textDecoration: 'none'}} to='/contact'><li>Contact</li></Link>
+            </ul>
+            <div className="footer-socials-icon">
+                <div className="footer-icons-container">
+                    <img src={instagram_icon} />
+                </div>
+                <div className="footer-icons-container">
+                    <img src={whatsapp_icon} />
+                </div>
+            </div>
+            <div className="footer-copyright">
+                <hr />
+                <p>Copyright @ 2023 - All Rights Reserved.</p>
+            </div>
+        </div>
     </div>
   )
 }
