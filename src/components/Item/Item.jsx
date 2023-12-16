620b14be5229ecaeb7d46e66e9cf637ad342b35e
SyedAreeb007 <144684190+SyedAreeb007@users.noreply.github.com>
2023-12-16 10:59:35 +0500

    Update Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Item = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Item = (props) => {
+    return (
+        <div className='item'>
+            <Link to={`/product/${props.id}`}><img src={props.image} /></Link>
+            <p>{props.name}</p>
+            <div className='item-prices'>
+                <div className='item-price-new'>
+                    Rs. {props.new_price}
+                </div>
+                <div className='item-price-old'>
+                    Rs. {props.old_price}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Item
