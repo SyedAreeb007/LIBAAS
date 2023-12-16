65836286fd4f450deb7b6357d826f1814fc835b2
Sadia Dayo <144684446+SadiaDayo@users.noreply.github.com>
2023-12-16 11:12:04 +0500

    Update NewCollections.jsx

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import new_collection from '../assets/new_collections'
+import Item from '../Item/Item'
 
-const NewCollections = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const NewCollections= () => {
+    return (
+        <div className='newcollections'>
+           <h1>NEW COLLECTIONS</h1>
+           <hr />
+           <div className="collections">
+                {new_collection.map((item,i)=>{
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                })}
+           </div>
+        </div>
+    )
 }
 
 export default NewCollections
