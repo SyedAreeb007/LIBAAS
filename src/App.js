35815af9e907dad4fea7fae310724dc2a60ae8a4
SyedAreeb007 <144684190+SyedAreeb007@users.noreply.github.com>
2023-12-19 10:57:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import LoginSignup from './pages/LoginSignup';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Footer from './components/Footer/Footer';
+import men_banner from './components/assets/banner_mens.png'
+import women_banner from './components/assets/banner_women.png'
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Navbar />
+        
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
