89d896c64fbaf5e5852a2af456ea7dfa7c8ef47f
Sadia Dayo <144684446+SadiaDayo@users.noreply.github.com>
2023-12-18 21:57:37 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,18 @@ function App() {
     <div>
       <BrowserRouter>
         <Navbar />
-        
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/product' element={<Product />} >
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
         <Footer/>
       </BrowserRouter>
     </div>
